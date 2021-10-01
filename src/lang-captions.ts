e231ef1fc50c6a67646776d2c22fd0592fcec5d6
Tewan <tewan@spycesoftware.com>
2021-10-01 12:52:33 +0200

    Separate caption color tag matching logic and test it

--- src/lang-captions.ts
+++ src/lang-captions.ts
@@ -9,6 +9,7 @@
 
 import { TextDocument, CancellationToken, DocumentColorProvider, Color, ColorPresentation, Range, ColorInformation, languages, SemanticTokensLegend, SemanticTokensBuilder, ExtensionContext, DocumentSelector } from "vscode";
 import { getDocument, KeyvalueDocumentFormatter } from "./keyvalue-document";
+import { populateColorTagMatches } from "./keyvalue-parser/kv-caption-tag-matches";
 import { KvTokensProviderBase, Processor } from "./keyvalue-parser/kv-token-provider-base";
 
 export const legend = new SemanticTokensLegend([
@@ -71,6 +72,7 @@ export class CaptionColorsProvider implements DocumentColorProvider {
         if(kvDoc == null) return [];
 
         const colorInfos: ColorInformation[] = [];
+        
         for(let i = 0; i < lines; i++) {
             if(cancellationToken.isCancellationRequested) break;
             
@@ -78,31 +80,12 @@ export class CaptionColorsProvider implements DocumentColorProvider {
             const kv = kvDoc.getKeyValueAt(i);
             if(kv == null) continue;
 
-            const clrMatches = [...kv.value.matchAll(/<clr:(\d{1,3}),(\d{1,3}),(\d{1,3})>/g)];
-            clrMatches.forEach(match => {
-                const color = new Color(parseInt(match[1]) / 255, parseInt(match[2]) / 255, parseInt(match[3]) / 255, 1.0);
-
-                const wholeString = match[0];
-                const posStart = kv.value.indexOf(wholeString);
-                const posEnd = posStart + wholeString.length;
-                const range = kv.valueRange.with(kv.valueRange.start.translate(0, posStart + 5), kv.valueRange.start.translate(0, posEnd - 1));
-                colorInfos.push(new ColorInformation(range, color));
-            });
-
-            const playerclrMatches = [...kv.value.matchAll(/<playerclr:(\d{1,3}),(\d{1,3}),(\d{1,3}):(\d{1,3}),(\d{1,3}),(\d{1,3})>/g)];
-            playerclrMatches.forEach(match => {
-                const color1 = new Color(parseInt(match[1]) / 255, parseInt(match[2]) / 255, parseInt(match[3]) / 255, 1.0);
-                const color2 = new Color(parseInt(match[4]) / 255, parseInt(match[5]) / 255, parseInt(match[6]) / 255, 1.0);
-
-                const start1 = kv.value.indexOf(":") + 1;
-                const end1 = kv.value.lastIndexOf(":");
-                const end2 = kv.value.lastIndexOf(match[6]) + match[6].length; // Javascript, why don't you just allow me to get the index of the match?
-
-                const range1 = kv.valueRange.with(kv.valueRange.start.translate(0, start1), kv.valueRange.start.translate(0, end1));
-                const range2 = kv.valueRange.with(kv.valueRange.start.translate(0, end1 + 1), kv.valueRange.start.translate(0, end2));
-                colorInfos.push(new ColorInformation(range1, color1), new ColorInformation(range2, color2));
-            });
-
+            const clrInfo = populateColorTagMatches(kv.value);
+            clrInfo.forEach((clr) => {
+                const colorInfo = new ColorInformation(kv.valueRange.with(kv.valueRange.start.translate(0, clr.start), kv.valueRange.start.translate(0, clr.end)), 
+                                                        new Color(clr.color.r / 255, clr.color.g / 255, clr.color.b / 255, 1.0));
+                colorInfos.push(colorInfo);
+            })
         }
 
         return colorInfos;
