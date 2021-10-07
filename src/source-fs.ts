ab21eed4ddc20656a87de717636bf5976a99afb4
Tewan <tewan@spycesoftware.com>
2021-10-07 11:03:05 +0200

    Actuall use the directoryName param in getParentDocumentDirectory, not a hard coded string wtf

--- src/source-fs.ts
+++ src/source-fs.ts
@@ -9,7 +9,7 @@
 
 export function getParentDocumentDirectory(path: string, directoryName: string): string | null {
 
-    const materialPathIndex = path.indexOf("materials") + "materials".length;
+    const materialPathIndex = path.indexOf(directoryName) + directoryName.length;
     if(materialPathIndex < 0) return null;
 
     return path.substring(0, materialPathIndex);
