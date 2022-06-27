d6fc8d335283d9ad6f77022b6b3da29830f7e673
Stefan Heinz <stefan.heinz.02@gmail.com>
2022-06-27 18:53:00 +0200

    Add separate jest config and update dependencies

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+    "transform": {
+        ".ts": "ts-jest"
+    },
+    "testRegex": "(/tests?/.*|\\.(test|spec))\\.(ts|js)$",
+    "moduleFileExtensions": [
+        "ts",
+        "js"
+    ],
+    "modulePathIgnorePatterns": [
+        "out"
+    ]
+}
