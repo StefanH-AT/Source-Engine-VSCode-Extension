6874076a229726af9b7235368a31b414b7f48255
Stefan Heinz <stefan.heinz.02@gmail.com>
2023-03-12 15:30:52 +0100

    Print performance output into debug output

--- src/performance.ts
+++ src/performance.ts
@@ -5,7 +5,7 @@ import * as main from "./main";
 const performanceObserver = new PerformanceObserver((list) => {
     const entry = list.getEntries()[0];
     if(main.config.get<boolean>("performance.log")) {
-        main.output.appendLine(`${entry.name}: ${entry.duration}ms`);
+        main.debugOutput.appendLine(`${entry.name}: ${entry.duration}ms`);
     }
 });
 
