3440a0d00056154812091d0bbac6f2d5a06ae2c9
Stefan Heinz <stefan.heinz.02@gmail.com>
2023-03-12 15:30:22 +0100

    Add hint to configure captions compiler

--- src/language/LangCaptions.ts
+++ src/language/LangCaptions.ts
@@ -3,7 +3,7 @@
 // Implementations of language utility providers for the captions language.
 // ==========================================================================
 
-import vscode from "vscode";
+import vscode, { window } from "vscode";
 import KvDocument from "./KvDocument";
 //import { KvDocumentFormatter } from "./KvFormatter";
 import { CaptionsColorsProvider } from "./CaptionsColorsProvider";
@@ -25,7 +25,32 @@ export function init(context: vscode.ExtensionContext): void {
     const captionsTokenProvider = vscode.languages.registerDocumentSemanticTokensProvider(selectorAll, new CaptionsSemanticTokenProvider(), KvDocument.tokenLegend);
     // TODO: Re-enable formatting here too
     //const captionsFormatter = vscode.languages.registerDocumentFormattingEditProvider(selectorAll, new KvDocumentFormatter());
-    context.subscriptions.push(captionsColors, captionsTokenProvider);
+
+    const onEditorChange = window.onDidChangeActiveTextEditor((editor: vscode.TextEditor | undefined): void => {
+        if (editor === undefined) return;
+        if (editor.document.languageId !== "captions") return;
+
+        recommendCompilerSetup(context);
+    });
+
+    context.subscriptions.push(captionsColors, captionsTokenProvider, onEditorChange);
 }
 
+function recommendCompilerSetup(context: vscode.ExtensionContext): void {
+    
+    const alreadyRecommended = context.globalState.get("captionsCompiler.recommended", false);
 
+    if(alreadyRecommended) return;
+
+    window.showInformationMessage("Captions can be compiled in the editor using the launch button on the top right. Do you want to set up the compiler?", 
+        "Set up compiler", "Ask later", "Don't ask again")
+        .then((value: string | undefined) => { 
+            if(value === "Set up compiler") {
+                vscode.commands.executeCommand("workbench.action.openSettings", "sourceEngine.captionCompiler");
+            }
+
+            if(value === "Ask later") return;
+
+            context.globalState.update("captionsCompiler.recommended", true);
+        });
+}
